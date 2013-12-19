381b474aac82216f27372c8f20d8d85bb3109e47
Owen Holloway <owen.holloway101@gmail.com>
2013-12-19 19:43:50 +1100

    completed question 3

--- problem3.cpp
+++ problem3.cpp
@@ -6,6 +6,7 @@ const unsigned long int number = 600851475143; //the number we are testing again
 unsigned long int finalNumber = number;
 
 int prime[primes];
+int factor[100];
 
 int currentPrime = 1;
 int testNo = 3; //As 2 is the first prime and 2%2 = 0 we start from 3
@@ -36,7 +37,15 @@ int main() {
 	}
 
 	//Prime generation ######################################################
-	
+
+	for (int i = 0; i < primes; i++) {
+		if (finalNumber % prime[i] == 0) {
+			finalNumber = finalNumber/prime[i];
+			std::cout << "prime factor: " << prime[i] << std::endl;
+		}
+
+	}
+
 	return 0;
 }
 
