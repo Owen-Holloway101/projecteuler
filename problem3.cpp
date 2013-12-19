013ff648bd920668b93cb61ad1d95bd9236e5abc
Owen Holloway <owen.holloway101@gmail.com>
2013-12-19 19:30:40 +1100

    Prime generation completed

--- problem3.cpp
+++ problem3.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 
-const int primes = 100;
-const int number = 600851475143; //the number we are testing against
-int finalNumber = number;
+const int primes = 10000;
+
+const unsigned long int number = 600851475143; //the number we are testing against
+unsigned long int finalNumber = number;
+
 int prime[primes];
 
-prime[0] = 2; //2 is the first prime as all other primes will come after
+int currentPrime = 1;
+int testNo = 3; //As 2 is the first prime and 2%2 = 0 we start from 3
 
 int main() {
-
-	int currentPrimes = 1;
-	int testNo = 3; //As 2 is the first prime and 2%2 = 0 we start from 3
  
-	while (currentPrimes < primes) {
+	//Prime generation ######################################################
 
+	prime[0] = 2; //2 is the first prime as all other primes will come after
+
+	while (currentPrime < primes) {
+
+		bool isPrime = true;
+		for (int i = 0; i < currentPrime; ++i) {
+			if ((float)(testNo %prime[i]) == float(0)) {
+				isPrime = false;
+			}
+		}
+
+		if (isPrime) {
+			prime[currentPrime] = testNo;
+			currentPrime++;
+			//std::cout << "Primes: " << currentPrime << std::endl;
+			std::cout << "Prime: " << testNo << std::endl;
+		}
+
+		testNo++;
 	}
+
+	//Prime generation ######################################################
 	
 	return 0;
 }
